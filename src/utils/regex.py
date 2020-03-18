24d5737dad57d9ce6595848f68fcb238f4d5fcbd
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2020-03-18 15:30:02 +0000

    Add regex to correct typos for common coronavirus terms

---
+++ src/utils/regex.py
@@ -0,0 +1,49 @@
+import re
+
+def coronavirus_misspellings_and_typos_regex():
+    """Return a regex you can use to replace misspellings of coronavirus with the correct spelling
+
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'caronavirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coranavirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'cornavirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'cornovirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronaviris')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'cornoavirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronavirius')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronavirous')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronaviru')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronaviurs')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronavius')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronoavirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronovirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coronvirus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'corona iris')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'corona virus')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'covid')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'corona')]
+    ['coronavirus']
+    >>> [re.sub(coronavirus_regex(), 'coronavirus', 'coron')]
+    ['coronavirus']
+    """
+    return "coronavirus|covid19|covid.19|caronavirus|coranavirus|cornavirus|cornovirus|coronaviris|cornoavirus|coronavirius|coronavirous|coronaviru|coronaviurs|coronavius|coronoavirus|coronovirus|coronvirus|corona iris|corona virus|covid|corona|coron"
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
