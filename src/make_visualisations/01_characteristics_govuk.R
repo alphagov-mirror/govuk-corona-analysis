4ba6f6dd9444b524de6b16b6f18eca1683613e68
avisionh <a_vision@hotmail.co.uk>
2020-03-20 14:16:59 +0000

    feat: Create time plot
    This is so we can show what it looks like for GOV.UK.

--- src/make_visualisations/01_characteristics_govuk.R
+++ src/make_visualisations/01_characteristics_govuk.R
@@ -1,13 +1,17 @@
-library(here)
-
 library(dplyr)
 library(stringr)
+library(lubridate)
 
 library(ggplot2)
 library(ggridges)
+library(govstyle)
 
-data_bq <- readRDS(file = here("data", "bg_pgviews_devicecategory.RDS"))
+# load data in
+data_bq <- readRDS(file = here::here("data", "bg_pgviews_devicecategory.RDS"))
 
+
+# Wrangle -----------------------------------------------------------------
+# add additional column to specify period
 data_bq <- data_bq %>%
   filter(!date %in% c(as.Date("2018-12-20"), as.Date("2019-12-20"))) %>%
   mutate(date_year = factor(x = date_year, levels = rev(sort(unique(date_year)))),
@@ -40,132 +44,60 @@ data_bq <- data_bq %>%
            (date >= "2020-01-21" & date <= "2020-02-20") ~ "2020 Jan/Feb",
            (date >= "2020-02-21" & date <= "2020-03-20") ~ "2020 Feb/Mar",
            TRUE ~ NA_character_)) %>%
-  mutate(date_period = factor(date_period, levels = unique(date_period))) 
+  mutate(date_period = factor(date_period, levels = unique(date_period)))
+
+# compute proportions
+data_bq <- data_bq %>%
+  group_by(date, date_period, datetime_hour, deviceCategory) %>% 
+  # sum of all pageviews for each day-hour for each category
+  summarise(total_pageviews = sum(x = as.numeric(pageviews), na.rm = TRUE)) %>%
+  # calculate proportion of hourly pageviews by category  
+  mutate(prop_pageviews = total_pageviews/(sum(total_pageviews)))
 
 
-library(assertthat)
+# Plot --------------------------------------------------------------------
 
-func_calc_hshares_by_monthCat <- function(data, 
-                                          characteristic_var=""){
-  
-  #'@param data (data.frame) : dataset  
-  #'@param characteristic_var (character string) : name of the characteristic variable 
-  #'@description Calculates shares/proportions of hourly pageviews for 
-  #'each characteristic category for each monthly period
-  #'
-  #'@return aggregated dataframe containing number and proportion of hourly pageviews by 'date_period' and characteristic_var 
-  
-  # checks
-  required_cols <- c("date_period", "datetime_hour", "pageviews")
-  
-  data %>%
-    assertthat::not_empty() %>%
-    assertthat::has_name(required_cols)
-  
-  
-  # to allow use with dplyr and ggplot2: covert to symbol and then unquote it using !!
-  sym_charac <- dplyr::sym(characteristic_var)
-  
-  agg_data = tryCatch({
-    
-    data %>%
-      group_by(
-        date_period, 
-        datetime_hour, 
-        !!sym_charac) %>% 
-      # sum of all pageviews for each day-hour for each category
-      summarise(
-        total_pageviews = sum(as.numeric(pageviews), na.rm = TRUE)
-      ) %>%
-      # calculate proportion of hourly pageviews by category  
-      mutate(
-        prop_pageviews = total_pageviews/(sum(total_pageviews))
-      )
-    
-  }, warning = function(w) {
-    message(paste0('** WARNING when applying `func_calc_hshares_by_monthCat()` with "', characteristic_var, '" **'))
-    print(w)
-    
-  }, error = function(e) {
-    message(paste0('** ERROR when applying `func_calc_hshares_by_monthCat()` with "', characteristic_var, '" **'))
-    print(e)
-    
-  }, finally={
-  })
-  
-  return(agg_data)
-  
-}
+  # Plot: Time ---------------------------------------------------------------
+data_timeplot <- data_bq %>% 
+  group_by(date, deviceCategory) %>% 
+  summarise(total_pageviews = sum(x = total_pageviews, na.rm = TRUE)) %>% 
+  mutate(prop_pageviews = total_pageviews/sum(x = total_pageviews))
+
+plot_devicecategory <- ggplot(data = data_timeplot, mapping = aes(x = date, y = prop_pageviews, colour = deviceCategory)) +
+  geom_line() +
+  facet_grid(rows = vars(deviceCategory)) +
+  geom_vline(xintercept = as.Date(c("2017-12-21", "2018-12-21", "2019-12-21")),
+             linetype = "dotted", colour = "black", size = 0.5) +
+  labs(title = "Time Plot of GOV.UK Daily Shares of Pageviews by device category", 
+       x = "Date", 
+       y = "Share of page views",
+       colour = guide_legend(title = "Key:")) +
+  theme_gov()
+
+# save to 16:9 aspect ratio suitable for full-bleed slides
+ggsave(filename = "reports/figures/devicecategory_time_all.jpg", plot = plot_devicecategory, width = 9, height = 5.0625, units = "in")
 
 
-
-
-# number of unique categories
-n_cols <- length(unique(data_bq[["characteristic_var"]]))
+  # Plot: Density ------------------------------------------------------------
 n_categories <- length(x = unique(x = data_bq$deviceCategory))
 
-data_bq %>%
+plot_devicecategory <- data_bq %>%
   #fill shades change according to x-values
   ggplot(mapping = aes(x = prop_pageviews, y = date_period, fill = stat(x))) + 
   geom_density_ridges_gradient(quantile_lines = TRUE, quantiles = 2, scale = 0.9) +
-  scale_fill_distiller(palette="Reds", direction = 1) +
-  scale_y_discrete(expand_scale(mult = c(0.01, 1))) +
-  scale_x_continuous(expand=c(0,0)) +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  #scale_y_discrete(expansion(mult = c(0.01, 1))) +
+  scale_x_continuous(expand = c(0,0)) +
   coord_cartesian(clip = "off") +
-  facet_wrap(reformulate(deviceCategory), ncol = n_categories) +
+  facet_wrap(reformulate(termlabels = "deviceCategory"), ncol = n_categories) +
   labs(
-    x = "share (i.e., proportion) of hourly page views",
-    y = "year and month",
-    title = paste("Density distribution of shares of hourly pageviews by", characteristic_var, "over monthly periods"),
-    subtitle = "Monthly periods start on the 21st of month 'k' and end on 20th of month 'k+1'",
-    caption = "20th of December was removed from all years"
-  )
+    x = "Share of hourly page views",
+    y = "Year and month",
+    title = paste("Density Distribution Plot of GOV.UK Hourly Shares of Pageviews by Device Category"),
+    caption = "Monthly periods start on the 21st of month 'k' and end on 20th of month 'k+1' \n20th of December was removed from all years"
+  ) +
+  theme_gov() +
+  theme(axis.title.y = element_blank())
 
-
-
-
-
-
-func_plot_densities_pgvwsShares_timeline(data=monthly_hshares_device,
-                                         characteristic_var="deviceCategory")
-func_plot_densities_pgvwsShares_timeline <- function(data, 
-                                                     characteristic_var=""){
-  
-  #'@param data : aggregated dataset with proportion of pageviews by `characteristic_var`, result of `func_calc_hshares_by_monthCat.R`
-  #'@param characteristic_var
-  #'@description Returns a ridgeline plot of the density distribution of proportions of hourly
-  #' pageviews by each characteristic category over months
-  
-  # checks
-  required_cols <- c("date_period", "datetime_hour", "total_pageviews", "prop_pageviews")
-  sapply(required_cols, function(col) if(!col %in% colnames(data)) stop(paste0("column", col, " is missing from dataset"))
-  )
-  
-  # to allow use with dplyr and ggplot2: covert to symbol and then unquote it using !!
-  sym_charac <- ggplot2::sym(characteristic_var)
-  
-  # number of unique categories
-  n_cols <- length(unique(data[[characteristic_var]]))
-  
-  # timeline of distributions of hourly pageview shares over months by category
-  data %>%
-    ggplot(., 
-           aes(x = prop_pageviews, y = date_period, fill=stat(x)) #fill shades change according to x-values
-    ) + 
-    ggridges::geom_density_ridges_gradient(
-      quantile_lines = TRUE, quantiles=2,
-      scale=0.9
-    ) +
-    scale_fill_distiller(palette="Reds", direction = 1) +
-    scale_y_discrete(expand_scale(mult = c(0.01, 1))) +
-    scale_x_continuous(expand=c(0,0)) +
-    coord_cartesian(clip = "off") +
-    facet_wrap(reformulate(characteristic_var), ncol=n_cols) +
-    labs(
-      x = "share (i.e., proportion) of hourly page views",
-      y = "year and month",
-      title = paste("Density distribution of shares of hourly pageviews by", characteristic_var, "over monthly periods"),
-      subtitle = "Monthly periods start on the 21st of month 'k' and end on 20th of month 'k+1'",
-      caption = "20th of December was removed from all years"
-    )
-}
+# save to 16:9 aspect ratio suitable for full-bleed slides
+ggsave(filename = "reports/figures/devicecategory_density_all.jpg", plot = plot_devicecategory, width = 9, height = 5.0625, units = "in")
