1aa6c430347252a68faaaea467d1c3b1edf9df0f
ff-l <33835310+ff-l@users.noreply.github.com>
2020-05-04 14:43:06 +0100

    commenting out for precommit

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from src.make_feedback_tool_data.regex_category_identification import (
-    regex_category_identification,
-    regex_group_verbs,
-    regex_for_theme
-)
+# from src.make_feedback_tool_data.regex_category_identification import (
+#     regex_category_identification,
+#     regex_group_verbs,
+#     regex_for_theme
+# )
