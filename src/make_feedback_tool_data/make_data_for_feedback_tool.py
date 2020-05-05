93f380e2a407823cd282d5f8c280850a5ee6cc41
ff-l <33835310+ff-l@users.noreply.github.com>
2020-05-05 16:26:47 +0100

    introduce logging

--- src/make_feedback_tool_data/make_data_for_feedback_tool.py
+++ src/make_feedback_tool_data/make_data_for_feedback_tool.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import logging.config
 import pandas as pd
 from tqdm import tqdm
 
@@ -18,7 +18,7 @@ def preproccess_filter_comment_text(full_df):
     :param full_df:
     :return:
     """
-
+    logger.info(f"Removing non-english and lengthy comments...")
     full_df['Q3_pii_removed'] = full_df['Q3_x'].progress_map(replace_pii_regex)
     full_df = full_df[(full_df.Q3_pii_removed.str.len() < 4000)]
 
@@ -42,6 +42,7 @@ def save_intermediate_df(processed_df, cache_pos_filename):
     :param cache_pos_filename:
     :return:
     """
+    logger.info(f"Saving preprocessed survey data at: {cache_pos_filename}...")
 
     processed_df['lemmas'] = processed_df['pos_tag'].progress_map(lambda x: [token[2] for sent in x for token in sent])
     processed_df['words'] = processed_df['pos_tag'].progress_map(lambda x: [token[0] for sent in x for token in sent])
@@ -49,7 +50,7 @@ def save_intermediate_df(processed_df, cache_pos_filename):
     processed_df.to_csv(cache_pos_filename, index=False)
 
 
-def extract_phrase_mentions(df, grammar_filename, cache_pos_filename):
+def extract_phrase_mentions(df, grammar_filename):
     """
 
     :param df:
@@ -61,6 +62,7 @@ def extract_phrase_mentions(df, grammar_filename, cache_pos_filename):
 
     parser = ChunkParser(grammar_filename)
 
+    logger.info("Detecting and extracting phrase-level mentions...")
     for vals in tqdm(df.pos_tag.values):
         sents = parser.extract_phrase(vals, True)
         phrase_mentions.append([])
@@ -91,6 +93,7 @@ def create_phrase_level_data(df, theme_col, phrase_type):
     :param phrase_type:
     :return:
     """
+    logger.info(f"Creating phrase level data for {theme_col}...")
     df[f'{phrase_type}_dict'] = df[[theme_col, "Q3_x_edit"]][:]. \
         progress_apply(lambda x: [find_needle(phrase, x[1].lower()) for _, phrase, _, _ in x[0]], axis=1)
     df[f'{phrase_type}_list'] = df[f'{phrase_type}_dict'].progress_map(lambda x: [value for phrase_dict in x for
@@ -106,6 +109,7 @@ def create_phrase_level_columns(df):
     :param df:
     :return:
     """
+    logger.info("Creating phrase level columns...")
     df["Q3_x_edit"] = df["Q3_x"].replace(np.nan, '', regex=True)
     df["Q3_x_edit"] = df["Q3_x_edit"].progress_map(lambda x: ' '.join(re.sub(r"\(|\)|\[|\]|\+", "", x).split()))
 
@@ -113,7 +117,7 @@ def create_phrase_level_columns(df):
     create_phrase_level_data(df, "theme_mentions_user", "user_phrases")
 
 
-def create_dataset(df, grammar_filename, cache_pos_filename):
+def create_dataset(survey_filename, grammar_filename, cache_pos_filename, output_filename):
     """
 
     :param df:
@@ -121,51 +125,60 @@ def create_dataset(df, grammar_filename, cache_pos_filename):
     :param cache_pos_filename:
     :return:
     """
-    df['pos_tag'] = df[['Q3_pii_removed', 'is_en']].progress_apply(
+
+    logger.info(f"Using grammar file: {grammar_filename}")
+
+    survey_data_df = pd.read_csv(survey_filename)
+    survey_data_df = preproccess_filter_comment_text(survey_data_df)
+
+    logger.info(f"Part of speech tagging comments...")
+    survey_data_df['pos_tag'] = survey_data_df[['Q3_pii_removed', 'is_en']].progress_apply(
         lambda x: part_of_speech_tag(x[0]) if x[1] else [],
         axis=1)
 
-    save_intermediate_df(df, cache_pos_filename)
+    extract_phrase_mentions(survey_data_df, grammar_filename)
+    create_phrase_level_columns(survey_data_df)
 
-    extract_phrase_mentions(df, grammar_filename, cache_pos_filename)
+    save_intermediate_df(survey_data_df, cache_pos_filename)
 
-    create_phrase_level_columns(df)
+    columns_to_keep = ['primary_key', 'intents_clientID', 'visitId', 'fullVisitorId',
+                       'hits_pagePath', 'Started', 'Ended', 'Q1_x', 'Q2_x', 'Q3_x_edit', 'Q4_x',
+                       'Q5_x', 'Q6_x', 'Q7_x', 'Q8_x', 'session_id', 'dayofweek', 'isWeekend',
+                       'hour', 'country', 'country_grouping', 'UK_region', 'UK_metro_area',
+                       'channelGrouping', 'deviceCategory',
+                       'total_seconds_in_session_across_days',
+                       'total_pageviews_in_session_across_days', 'finding_count',
+                       'updates_and_alerts_count', 'news_count', 'decisions_count',
+                       'speeches_and_statements_count', 'transactions_count',
+                       'regulation_count', 'guidance_count', 'business_support_count',
+                       'policy_count', 'consultations_count', 'research_count',
+                       'statistics_count', 'transparency_data_count',
+                       'freedom_of_information_releases_count', 'incidents_count',
+                       'done_page_flag', 'count_client_error', 'count_server_error',
+                       'ga_visit_start_timestamp', 'ga_visit_end_timestamp',
+                       'intents_started_date', 'events_sequence', 'search_terms_sequence',
+                       'cleaned_search_terms_sequence', 'top_level_taxons_sequence',
+                       'page_format_sequence', 'Sequence', 'PageSequence', 'flag_for_criteria',
+                       'full_url_in_session_flag', 'UserID', 'UserNo', 'Name', 'Email',
+                       'IP Address', 'Unique ID', 'Tracking Link', 'clientID', 'Page Path',
+                       'Q1_y', 'Q2_y', 'Q3_y', 'Q4_y', 'Q5_y', 'Q6_y', 'Q7_y', 'Q8_y',
+                       'Started_Date', 'Ended_Date', 'Started_Date_sub_12h', 'phrases', 'user_phrases']
 
-    df_sub = df[['primary_key', 'intents_clientID', 'visitId', 'fullVisitorId',
-                 'hits_pagePath', 'Started', 'Ended', 'Q1_x', 'Q2_x', 'Q3_x_edit', 'Q4_x',
-                 'Q5_x', 'Q6_x', 'Q7_x', 'Q8_x', 'session_id', 'dayofweek', 'isWeekend',
-                 'hour', 'country', 'country_grouping', 'UK_region', 'UK_metro_area',
-                 'channelGrouping', 'deviceCategory',
-                 'total_seconds_in_session_across_days',
-                 'total_pageviews_in_session_across_days', 'finding_count',
-                 'updates_and_alerts_count', 'news_count', 'decisions_count',
-                 'speeches_and_statements_count', 'transactions_count',
-                 'regulation_count', 'guidance_count', 'business_support_count',
-                 'policy_count', 'consultations_count', 'research_count',
-                 'statistics_count', 'transparency_data_count',
-                 'freedom_of_information_releases_count', 'incidents_count',
-                 'done_page_flag', 'count_client_error', 'count_server_error',
-                 'ga_visit_start_timestamp', 'ga_visit_end_timestamp',
-                 'intents_started_date', 'events_sequence', 'search_terms_sequence',
-                 'cleaned_search_terms_sequence', 'top_level_taxons_sequence',
-                 'page_format_sequence', 'Sequence', 'PageSequence', 'flag_for_criteria',
-                 'full_url_in_session_flag', 'UserID', 'UserNo', 'Name', 'Email',
-                 'IP Address', 'Unique ID', 'Tracking Link', 'clientID', 'Page Path',
-                 'Q1_y', 'Q2_y', 'Q3_y', 'Q4_y', 'Q5_y', 'Q6_y', 'Q7_y', 'Q8_y',
-                 'Started_Date', 'Ended_Date', 'Started_Date_sub_12h', 'phrases', 'user_phrases']]
-
-    df_sub.rename(columns={'Q3_x_edit': 'Q3_x'}, inplace=True)
-    df_sub.to_csv(os.path.join(DATA_DIR, 'uis_20200401_20200409_phrases_user_groups.csv'), index=False)
+    survey_data_df.rename(columns={'Q3_x_edit': 'Q3_x'}, inplace=True)
+    survey_data_df[columns_to_keep].to_csv(os.path.join(DATA_DIR, output_filename), index=False)
 
 
 if __name__ == "__main__":
-    DATA_DIR = os.getenv("DATA_DIR")
+    log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logging.conf')
+    logging.config.fileConfig(log_file_path)
+    logger = logging.getLogger(__name__)
+
+    DATA_DIR = os.getenv("DIR_DATA")
     filename = os.path.join(DATA_DIR, "")
 
-    grammar_filename = ""
-    cache_pos_filename = ""
+    survey_data_filename = ''
+    grammar_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "grammar.txt")
+    cache_pos_filename = 'uis_20200401_20200409_cache.csv'
+    output_filename = 'uis_20200401_20200409_phrases_user_groups.csv'
 
-    survey_data = pd.read_csv(filename)
-
-    filtered_df = preproccess_filter_comment_text(survey_data)
-    create_dataset(filtered_df, grammar_filename, cache_pos_filename)
+    create_dataset(survey_data_filename, grammar_filename, cache_pos_filename, output_filename)
