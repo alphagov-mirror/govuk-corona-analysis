972bef7f67a47287e39c9560334238a1b8062a77
Eric Young <eric.young@digital.cabinet-office.gov.uk>
2020-05-06 11:56:12 +0100

    Add type hints to `Chunk` class
    Also add `Chunk` to `src/__init__.py` script.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from src.make_feedback_tool_data.chunk import Chunk
 from src.make_feedback_tool_data.preprocess import PreProcess, PII_REGEX
 from src.make_feedback_tool_data.regex_category_identification import (
     regex_category_identification,
@@ -5,4 +6,4 @@ from src.make_feedback_tool_data.regex_category_identification import (
     regex_for_theme
 )
 
-__all__ = ["PreProcess", "PII_REGEX", "regex_category_identification", "regex_group_verbs", "regex_for_theme"]
+__all__ = ["Chunk", "PreProcess", "PII_REGEX", "regex_category_identification", "regex_group_verbs", "regex_for_theme"]
--- src/make_feedback_tool_data/chunk.py
+++ src/make_feedback_tool_data/chunk.py
@@ -1,9 +1,10 @@
+from typing import List, Tuple
 import re
 
 
 class Chunk:
 
-    def __init__(self, label, tokens, indices):
+    def __init__(self, label: str, tokens: List[Tuple[str, str, str]], indices: List[int]) -> None:
         """Helper class to extract and combine useful tokens/lemmas based on part-of-speech (POS) tag.
 
         :param label: A POS tag.
@@ -19,7 +20,7 @@ class Chunk:
         self.important_lemma = self.important_lemma()
         self.important_word = self.important_word()
 
-    def text(self):
+    def text(self) -> str:
         """Combine tokens into a text string.
 
         :return: A string of all tokens delimited by a space.
@@ -27,7 +28,7 @@ class Chunk:
         """
         return " ".join([w for w, _, _ in self.tokens])
 
-    def lemma(self):
+    def lemma(self) -> str:
         """Combine lemmas of the tokens into a string.
 
         :return: A string of all lemmas delimited by a space.
@@ -35,7 +36,7 @@ class Chunk:
         """
         return " ".join([l for _, _, l in self.tokens])
 
-    def tagable_words(self):
+    def tagable_words(self) -> List[Tuple[str, str]]:
         """Get each token and its parts-of-speech (POS) tag, if the POS tag is a noun or verb.
 
         :return: A list of two-element tuples, where the first element is the token, and the second element is the
@@ -44,7 +45,7 @@ class Chunk:
         """
         return [(w, pos) for w, pos, _ in self.tokens if re.search(r"(NN)|(VB)", pos)]
 
-    def important_word(self):
+    def important_word(self) -> str:
         """Get a string of all important tokens, based on their part-of-speech (POS) tag.
 
         Important tokens are defined as being nouns, verbs, adjectives, or cardinal numbers.
@@ -54,7 +55,7 @@ class Chunk:
         """
         return " ".join([w for w, pos, _ in self.tokens if re.search(r"(NN)|(VB)|(JJ)|(CD)", pos)])
 
-    def important_lemma(self):
+    def important_lemma(self) -> str:
         """Get a string of all important lemmas, based on their part-of-speech (POS) tag.
 
         Important lemmas are defined as being nouns, verbs, adjectives, or cardinal numbers.
