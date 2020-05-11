e6396a1bbcaf4bb720443001b0bf26843a9273c8
Eric Young <eric.young@digital.cabinet-office.gov.uk>
2020-05-11 12:04:00 +0100

    Add `ChunkParser` to `src/__init__.py`

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,7 @@ from src.make_feedback_tool_data.regex_category_identification import (
     regex_group_verbs,
     regex_for_theme
 )
+from src.make_feedback_tool_data.text_chunking import ChunkParser
 
-__all__ = ["Chunk", "PreProcess", "PII_REGEX", "preproccess_filter_comment_text", "regex_category_identification",
-           "regex_group_verbs", "regex_for_theme", "save_intermediate_df"]
+__all__ = ["Chunk", "ChunkParser", "PreProcess", "PII_REGEX", "preproccess_filter_comment_text",
+           "regex_category_identification", "regex_group_verbs", "regex_for_theme", "save_intermediate_df"]
