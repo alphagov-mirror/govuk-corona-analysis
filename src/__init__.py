960bf18454b927ba23df8af59df021af8a3c8af6
Eric Young <eric.young@digital.cabinet-office.gov.uk>
2020-05-11 12:02:53 +0100

    Add `save_intermediate_df` to `src/__init__.py`

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,8 @@
 from src.make_feedback_tool_data.chunk import Chunk
-from src.make_feedback_tool_data.make_data_for_feedback_tool import preproccess_filter_comment_text
+from src.make_feedback_tool_data.make_data_for_feedback_tool import (
+    preproccess_filter_comment_text,
+    save_intermediate_df
+)
 from src.make_feedback_tool_data.preprocess import PreProcess, PII_REGEX
 from src.make_feedback_tool_data.regex_category_identification import (
     regex_category_identification,
@@ -8,4 +11,4 @@ from src.make_feedback_tool_data.regex_category_identification import (
 )
 
 __all__ = ["Chunk", "PreProcess", "PII_REGEX", "preproccess_filter_comment_text", "regex_category_identification",
-           "regex_group_verbs", "regex_for_theme"]
+           "regex_group_verbs", "regex_for_theme", "save_intermediate_df"]
